04053fbc8e614fdae8351ee2030183582e5a2295
Bekir Okić <okicbekir@gmail.com>
2021-05-29 21:56:11 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from './components/About';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 //scroll to top
 function App() {
@@ -14,10 +15,9 @@ function App() {
           <Route exact path="/" component={Home}/>
           <Route exact path="/projects" component={Projects}/>
           <Route exact path="/about" component={About}/>
+          <Route exact path="/contact" component={Contact}/>
         </Switch>
       </Router>
-      <Projects />
-      <About />
     </>
   );
 }
