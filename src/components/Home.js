5ba079f30c8313cdea2b7d941ccfcf948252afca
Bekir Okić <okicbekir@gmail.com>
2021-05-29 21:56:46 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { FaArrowLeft, FaArrowRight, FaDoorClosed } from 'react-icons/fa';
+import { FaFacebook, FaInstagram, FaArrowLeft, FaArrowRight, FaDoorClosed } from 'react-icons/fa';
 import { HiArrowNarrowRight } from 'react-icons/hi';
 import { AiFillSetting } from 'react-icons/ai';
 import { GiWindow } from 'react-icons/gi';
@@ -64,14 +64,14 @@ function Home() {
                     <div className="row gx-0">
                         <div className="col-lg-6 colAbout">
                             <div data-aos="fade-right" className="aboutTitle">
-                                <h6>ovo je naš profil</h6>
+                                <h6>DOBRODOŠLI</h6>
                                 <h1>Saznajte više <br /><span>o nama</span></h1>
                                 <div className="aboutText">
-                                    <h3>OVO JE NAŠ PROFIL</h3>
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, and scrambled it to make a type specimen book.</p>
+                                    <h3>PO ČEMU SMO JEDINSTVENI</h3>
+                                    <p>Rico-plast  Živinice na prostorima BiH posluje već 20 godina, tijekom kojih se razvio u sistemu prodaje mašina za proizvodnju PVC i ALU mašina značajnim iskustvom u savjetovanju, razvoju i uvođenju. Fokusirani smo na razvoj dugoročne stolarije i održivih mašina. Pružamo kvalitetne proizvode uvežene iz Njemačke, Turske i drugih država.</p>
                                 </div>
                                 <div>
-                                    <a className="btnAbout" href="/">Naručite proizvod</a>
+                                    <a className="btnAbout" href="mailto: rico-plast@gmail.com">Naručite proizvod</a>
                                 </div>
                             </div>
                         </div>
@@ -89,9 +89,9 @@ function Home() {
                     <div className="row gx-0 justifyCenter">
                         <div className="col-lg-6">
                             <div className="offerTitle">
-                                <h6 className="heading">SERVICES</h6>
+                                <h6 className="heading">Usudite se sanjati o modernom domu.</h6>
                                 <h3 className="offerHeader">ŠTA NUDIMO</h3>
-                                <p>Provjerite naše proizvode, vjerovatno ćete naći nešto što Vam odgovara.</p>
+                                <p>Drugi o tome pričaju, a mi to radimo. Već 20 godina.</p>
                             </div>
                         </div>
                     </div>
@@ -102,7 +102,7 @@ function Home() {
                             <div className="servicesBox" onClick={() => setModalIsOpen(true)}>
                                 <AiFillSetting className="commonIcons" />
                                 <div className="servicesBoxHeader">PVC/ALU MAŠINE CNC</div>
-                                <div className="servicesBoxPara">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
+                                <div className="servicesBoxPara">U našoj punudi možete pronaći jednoglavne i dvoglavne mašine.</div>
                             </div>
                             <Modal isOpen={modalIsOpen}>
                                 <button className="modalButton" onClick={handleClose}>X</button>
@@ -124,7 +124,7 @@ function Home() {
                             <div className="servicesBox" onClick={() => setModalIsOpen2(true)}>
                                 <GiWindow className="commonIcons" />
                                 <div className="servicesBoxHeader">PVC STOLARIJA</div>
-                                <div className="servicesBoxPara">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
+                                <div className="servicesBoxPara">Stolarija nezaboravne kvalitete i složene strukture.</div>
                             </div>
                             <Modal isOpen={modalIsOpen2}>
                                 <button className="modalButton" onClick={handleClose2}>X</button>
@@ -146,7 +146,7 @@ function Home() {
                             <div className="servicesBox" onClick={() => setModalIsOpen3(true)}>
                                 <FaDoorClosed className="commonIcons" />
                                 <div className="servicesBoxHeader">ULAZNA VRATA</div>
-                                <div className="servicesBoxPara">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
+                                <div className="servicesBoxPara">Jungmeister ulazna vrata sa modelima za svačiji ukus.</div>
                             </div>
                             <Modal isOpen={modalIsOpen3}>
                                 <button className="modalButton" onClick={handleClose3}>X</button>
@@ -168,7 +168,7 @@ function Home() {
                             <div className="servicesBox" onClick={() => setModalIsOpen4(true)}>
                                 <FaDoorClosed className="commonIcons" />
                                 <div className="servicesBoxHeader">SOBNA VRATA</div>
-                                <div className="servicesBoxPara">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
+                                <div className="servicesBoxPara">Vrata sa kreativnim dizajnom po svjetskim trendovima.</div>
                             </div>
                             <Modal isOpen={modalIsOpen4}>
                                 <button className="modalButton" onClick={handleClose4}>X</button>
@@ -190,7 +190,7 @@ function Home() {
                             <div className="servicesBox" onClick={() => setModalIsOpen5(true)}>
                                 <AiFillSetting className="commonIcons " />
                                 <div className="servicesBoxHeader">KOMPRESORI</div>
-                                <div className="servicesBoxPara">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
+                                <div className="servicesBoxPara">Dizajnirani da bez problema služe kupcima dugo godina.</div>
                             </div>
                             <Modal isOpen={modalIsOpen5}>
                                 <button className="modalButton" onClick={handleClose5}>X</button>
@@ -317,7 +317,7 @@ function Home() {
                                             <input type="text" placeholder="Upišite Poruku" name="message" className="contactForm" required="true" />
                                         </div>
                                         <div>
-                                            <a className="btnAbout" href="/">Pošalji</a>
+                                            <a className="btnAbout" href="mailto: rico-plast@gmail.com">Pošalji</a>
                                         </div>                              
                             </form>
                         </div>
@@ -336,7 +336,7 @@ function Home() {
                                     <img className="logo" src={img8}/>
                                 </a>
                             </div>
-                            <p className="footerText footerText1">Provjerite naše proizvode, vjerovatno ćete naći nešto što Vam odgovara. Poštujemo dogovor, zahtjeve kupca i investitora. Poštujemo rokove, izvodimo radove kvalitetno, koristimo kvalitetne materijale. A, radimo brzo!</p>
+                            <p className="footerText footerText1">Provjerite naše proizvode, vjerovatno ćete naći nešto što Vam odgovara. Poštujemo dogovor, zahtjeve kupca i investitora. Poštujemo rokove, izvodimo radove kvalitetno, koristimo kvalitetne materijale. Radimo brzo!</p>
                         </div>
                         <div className="col-lg-4 linkColumn">
                             <h6 className="contactText">NAŠE STRANICE</h6>
@@ -351,15 +351,28 @@ function Home() {
                             <h6 className="contactText">KONTAKT INFO</h6>
                             <div className="textFooterList">
                                 <p className="footerText">Živinice, Bosna i Hercegovina</p>
-                                <p className="footerText">+387 61 111 222</p>
+                                <p className="footerText">064 41 74 841</p>
+                                <p className="footerText">061 392 925</p>
+                                <p className="footerText">060 33 96 286</p>
                                 <p className="footerText">rico-plast@gmail.com</p>
-                                <p className="footerText">Facebook</p>
-                                <p className="footerText">Instagram</p>
+                                <a href="https://www.facebook.com/">
+                                    <FaFacebook className="social-media" />
+                                </a>
+                                <a href="https://www.instagram.com/">
+                                    <FaInstagram className="social-media" />
+                                </a>
                             </div>
                         </div>
                     </div>
                 </div>
             </footer>
+            <section className="copyright-section">
+                    <div className="row gx-0 copyrightRow">
+                        <div className="col-lg-12">
+                            <p className="copyright-text">By Programmer: Bekir Okić</p>
+                        </div>
+                    </div>
+            </section>
         </>
     )
 }
