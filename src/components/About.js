18e3aeb5efe7dd61165022eccd060ecb2a2073bc
Bekir Okić <okicbekir@gmail.com>
2021-06-01 17:59:17 +0200

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { FaFacebook, FaInstagram, FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 import img from '../components/2.jpg';
-import img8 from '../components/wintech2.png';
+import imgLogo from '../components/logo.png';
 
 const About = () => {
     return (
         <>
-            <section className="products-page" id="about">
+            <section className="products-page" id="about" data-aos="fade-up">
                 <div className="container">
                     <div className="row gx-0">
                         <div className="headerRow">
@@ -25,15 +25,15 @@ const About = () => {
                             <div className="article-list">
                                 <div className="row gx-0 aboutRow">
                                     <div className="col-md-12 aboutCol aboutCol1">
-                                        <div className="article">
-                                            <h3>OVO JE NAŠ PROFIL</h3>
+                                        <div className="article" data-aos="flip-right" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
+                                            <h3>DOBRODOŠLICA</h3>
                                             <p className="article-introtext">Poštovani, Želimo Vam se zahvaliti na interesu koji pokazujete za našu kompaniju. Veliko nam je zadovoljstvo predstaviti novu web-stranicu kojom Vas pozivamo da se uvjerite u unaprijeđene stare, ali i nove mogućnosti našeg asortimana proizvoda i usluga i prodaja kao i u nova dostignuća našeg kreativnog tima. Već 20 godina inspirirani smo prateći trendove i nove modele od mašina za proizvodnju PVC/ALU stolarije do CNC mašina, kao i kompresor od običnih do vičanih, novih modela sobnih vrata, profili od 5 i 6 komora, komarnici i roletne. Neprekidno radimo na tome da ponudimo proizvode i usluge koji odgovaraju tržištu.</p>
                                         </div>
 
                                     </div>
                                     <div className="col-md-12 aboutCol aboutCol2">
-                                        <div className="article2">
-                                            <h3>OVO JE NAŠ PROFIL 2</h3>
+                                        <div className="article2" data-aos="flip-right" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
+                                            <h3>NAŠA PRIČA</h3>
                                             <p className="article-introtext2">Kompanija „Rico-plast d.o.o.“ sa sjedištem u Živinicama, osnovana je 2001. godine i uspješno posluje već 20 godina. Kompanija je specijalizirana za prodaju jednoglavnih, dvoglavnih i CNC mašina za proizvodnju PVC/ALU stolarije. Također nudimo i sobna vrata, pvc stolariju, roletne i komarnike. Otvoreni smo za usluge koje Vam trebaju. Priča o Kompaniji “Rico-plast” d.o.o. ujedno je i uzbudljiva priča. Stotine ljudi koji u svom radu uvijek nastoje pomjerati granice kreativnosti, zalaganja i timskog rada.</p>
                                         </div>
                                     </div>
@@ -42,52 +42,6 @@ const About = () => {
                         </div>
                     </div>
                 </div>
-            </section>
-            <footer className="footer-area">
-                <div className="container">
-                    <div className="row gx-0 justifyCenter">
-                        <div className="col-lg-4">
-                            <div className="logoFooter">
-                                <a href="#">
-                                    <img className="logo" src={img8}/>
-                                </a>
-                            </div>
-                            <p className="footerText footerText1">Provjerite naše proizvode, vjerovatno ćete naći nešto što Vam odgovara. Poštujemo dogovor, zahtjeve kupca i investitora. Poštujemo rokove, izvodimo radove kvalitetno, koristimo kvalitetne materijale. Radimo brzo!</p>
-                        </div>
-                        <div className="col-lg-4 linkColumn">
-                            <h6 className="contactText">NAŠE STRANICE</h6>
-                            <ul className="footerList">
-                                <li><a href="#" className="listItem">Početna</a></li>
-                                <li><a href="#" className="listItem">Proizvodi</a></li>
-                                <li><a href="#" className="listItem">O nama</a></li>
-                                <li><a href="#" className="listItem">Kontakt</a></li>
-                            </ul>
-                        </div>
-                        <div className="col-lg-3 linkColumn2">
-                            <h6 className="contactText">KONTAKT INFO</h6>
-                            <div className="textFooterList">
-                                <p className="footerText">Živinice, Bosna i Hercegovina</p>
-                                <p className="footerText">064 41 74 841</p>
-                                <p className="footerText">061 392 925</p>
-                                <p className="footerText">060 33 96 286</p>
-                                <p className="footerText">rico-plast@gmail.com</p>
-                                <a href="https://www.facebook.com/Rico-plast-106317514411877">
-                                    <FaFacebook className="social-media" />
-                                </a>
-                                <a href="https://www.instagram.com/">
-                                    <FaInstagram className="social-media" />
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </footer>
-            <section className="copyright-section">
-                    <div className="row gx-0 copyrightRow">
-                        <div className="col-lg-12">
-                            <p className="copyright-text">By Programmer: Bekir Okić</p>
-                        </div>
-                    </div>
             </section>
         </>
     )
