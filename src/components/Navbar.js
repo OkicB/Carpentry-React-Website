922ce4397f0ce4f5fee76157fde37cb040840731
Bekir Okić <okicbekir@gmail.com>
2021-06-02 11:09:59 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,5 @@
 import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-//import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { HashLink as Link } from 'react-router-hash-link';
-import Projects from './Projects';
 import imgLogo from '../components/logo.png';
 import { CgMenuRight } from 'react-icons/cg';
 
@@ -10,23 +7,23 @@ const Navbar = () => {
     return (
         <div className="navbarFirst">
             <nav className="navbar navbar-expand-lg">
-                <a className="navbar-brand imgLogo" href="#home"><Link to="/"><img src={imgLogo}/></Link></a>
+                <a className="navbar-brand imgLogo" href="#home"><Link to="/#home"><img src={imgLogo}/></Link></a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <CgMenuRight className="menu-icon" />
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#home"><Link className="navLink" to="/">POČETNA</Link></a>
+                            <a className="nav-link"><Link className="navLink" to="./#home">POČETNA</Link></a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#projects"><Link className="navLink" to='./Projects'>PROIZVODI</Link></a>
+                            <a className="nav-link"><Link className="navLink" to='./Projects#projects'>PROIZVODI</Link></a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#about"><Link className="navLink" to='./About'>O NAMA</Link></a>
+                            <a className="nav-link"><Link className="navLink" to='./About#about'>O NAMA</Link></a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#contact"><Link className="navLink" to='./Contact'>KONTAKT</Link></a>
+                            <a className="nav-link"><Link className="navLink" to='./Contact#contact'>KONTAKT</Link></a>
                         </li>
                     </ul>
                 </div>
